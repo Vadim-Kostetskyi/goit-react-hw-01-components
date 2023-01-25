a684ab7476a9328f32b701872d4cdeca24ccbbbb
storm2107 <113288447+storm2107@users.noreply.github.com>
2023-01-25 13:33:57 +0200

    1

---
+++ src/components/task 1/Profile.jsx
@@ -0,0 +1,40 @@
+import css from "./Profile.module.css";
+import PropTypes from "prop-types";
+
+const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt="User avatar" className={css.avatar} />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+
+      <ul className={css.stats}>
+        <li className={css.item}>
+          <span className={css.label}>Followers</span> <br />
+          <span className={css.quantity}>{stats.followers}</span>
+        </li>
+        <li className={css.item}>
+          <span className={css.label}>Views</span> <br />
+          <span className={css.quantity}>{stats.views}</span>
+        </li>
+        <li className={css.item}>
+          <span className={css.label}>Likes</span> <br />
+          <span className={css.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.object,
+};
+
+export default Profile;
