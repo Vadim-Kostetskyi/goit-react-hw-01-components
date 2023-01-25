97ef6f6c65d7f0fb81ce8a86224c3e2620289a5e
storm2107 <113288447+storm2107@users.noreply.github.com>
2023-01-25 15:55:51 +0200

    12

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
-import user from './task 1/user.json';
-// import data from './task 2/data.json';
-import friends from './task 3/friends.json';
-import transactions from './task 4/transactions.json';
+import user from './task1/user.json';
+import data from './task2/data.json';
+import friends from './task3/friends.json';
+import transactions from './task4/transactions.json';
 
-import Profile from './task 1/Profile';
-// import Statistics from './task 2/Statistic';
-import FriendList from './task 3/FriendList';
-import TransactionHistory from './task 4/Transitions';
+import Profile from './task1/Profile';
+import Statistics from './task2/Statistic';
+import FriendList from './task3/FriendList';
+import TransactionHistory from './task4/Transitions';
 
 export const App = () => {
   return (
@@ -18,8 +18,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      {/* <Statistics title="Upload stats" stats={data} />
-      <Statistics stats={data} /> */}
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
     </div>
