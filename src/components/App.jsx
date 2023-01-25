456b6bb080bda8067566da5f45b36a38deff5443
storm2107 <113288447+storm2107@users.noreply.github.com>
2023-01-25 14:31:27 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import user from './task 1/user.json';
 // import data from './task 2/data.json';
 // import friends from './task 3/friends.json';
-// import transactions from './task 4/transactions.json';
+import transactions from './task 4/transactions.json';
 
 import Profile from './task 1/Profile';
 // import Statistics from './task 2/Statistic';
 // import FriendList from './task 3/AllFriends';
-// import TransactionHistory from './task 4/Transitions';
+import TransactionHistory from './task 4/Transitions';
 
 export const App = () => {
   return (
@@ -21,7 +21,7 @@ export const App = () => {
       {/* <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} /> */}
       {/* <FriendList friends={friends} /> */}
-      {/* <TransactionHistory items={transactions} /> */}
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
